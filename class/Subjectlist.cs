04377f4d9d189375ae5d99d19b5da628e4113ba0
JidapaBubu <jidapa4499@gmail.com>
2022-11-28 15:30:05 +0700

    Update Subj.

--- class/Subjectlist.cs
+++ class/Subjectlist.cs
@@ -7,8 +7,14 @@ class SubjectList {
         this.subjectList = new List<Subject>();
     }
 
-    public void AddNewPerson(Subject subject) {
+    public void AddNewSubject(Subject subject) {
         this.subjectList.Add(subject);
     }
 
+    public void printSubject(){
+        Console.WriteLine("***** Avelible Subject List *****");
+        foreach(Subject subject in this.subjectList) {
+            Console.WriteLine("ID: "+subject.getID()+"  Name: "+subject.GetName());
+        }
+    }
 }
